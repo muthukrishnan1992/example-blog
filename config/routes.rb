511a9a0ad83085848418a17e5c9bf9a3a7e4eedc
alpha_app <muthunew13@gmail.com>
2016-05-20 21:20:40 +0530

    edit and updated also comments completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   
-     resources :posts
-    # do
-    #resources :comments
-  #end
+     resources :posts do
+    resources :comments
+  end
   root "posts#index"
 
  # get '/about', to: 'pages#about'
